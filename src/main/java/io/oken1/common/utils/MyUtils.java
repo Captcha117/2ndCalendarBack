e5afabad1fb0a43640f3572019a6c739267197ca
Captcha117 <4463215@qq.com>
2025-01-14 23:30:40 +0800

    feat: 根据文章ID获取活动详情

--- src/main/java/io/oken1/common/utils/MyUtils.java
+++ src/main/java/io/oken1/common/utils/MyUtils.java
@@ -1,8 +1,9 @@
 package io.oken1.common.utils;
 
+import org.apache.commons.codec.digest.DigestUtils;
 import org.apache.commons.lang.StringUtils;
 
-import java.util.UUID;
+import java.util.*;
 
 public class MyUtils {
     public static String myUUID() {
@@ -24,4 +25,36 @@ public class MyUtils {
     public static String nvl(String str1, String str2, String str3) {
         return str1 == null ? str3 : str2;
     }
+
+    private static final String API_SALT = "xV8v4Qu54lUKrEYFZkJhB8cuOh9Asafs";
+
+    public static String GetDS(Map<String, String> query, String body) {
+        String i = "" + System.currentTimeMillis() / 1000;
+        String r = MyRandom(6);
+        String q = "";
+        if (query != null) {
+            Set<String> queryKeys = query.keySet();
+            List<String> que = new ArrayList<>();
+            for (String queryKey : queryKeys) {
+                que.add(queryKey + "=" + query.get(queryKey));
+            }
+            q = StringUtils.join(que, "&");
+        }
+        String c = DigestUtils.md5Hex("salt=" + API_SALT + "&t=" + i + "&r=" + r + "&b=" + body + "&q=" + q);
+        return i + "," + r + "," + c;
+    }
+
+    public static String MyRandom(int length) {
+        String characters = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+        StringBuilder sb = new StringBuilder();
+        Random random = new Random();
+
+        for (int i = 0; i < length; i++) {
+            int index = random.nextInt(characters.length());
+            sb.append(characters.charAt(index));
+        }
+
+        return sb.toString();
+    }
+
 }
--- src/main/java/io/oken1/modules/calendar/controller/CalendarEventController.java
+++ src/main/java/io/oken1/modules/calendar/controller/CalendarEventController.java
@@ -1,16 +1,22 @@
 package io.oken1.modules.calendar.controller;
 
-import java.util.Arrays;
-import java.util.LinkedHashMap;
-import java.util.List;
-import java.util.Map;
+import java.io.IOException;
+import java.util.*;
 
+import cn.hutool.core.util.StrUtil;
+import io.oken1.common.utils.MyUtils;
 import io.oken1.modules.calendar.dao.CalendarEventDao;
 import io.oken1.modules.calendar.entity.CalendarEventRewardEntity;
 import io.oken1.modules.calendar.model.EventModel;
 import io.oken1.modules.calendar.service.CalendarEventRewardService;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
+import org.apache.http.HttpEntity;
+import org.apache.http.HttpResponse;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClients;
+import org.apache.http.util.EntityUtils;
 import org.apache.shiro.authz.annotation.RequiresPermissions;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
@@ -73,8 +79,12 @@ public class CalendarEventController {
     @ApiOperation("保存")
     @PostMapping("/save")
     // @RequiresPermissions("calendar:calendarevent:save")
-    public R save(@RequestBody CalendarEventEntity calendarEvent) {
+    public R save(@RequestBody EventModel calendarEvent) {
+        List<CalendarEventRewardEntity> rewardList = calendarEvent.getRewardList();
         calendarEventService.save(calendarEvent);
+        
+        rewardList.forEach(x -> x.setEventId(calendarEvent.getId()));
+        calendarEventRewardService.saveBatch(rewardList);
 
         return R.ok();
     }
@@ -148,5 +158,39 @@ public class CalendarEventController {
         List<EventModel> data = calendarEventService.getByIds(ids);
 
         return R.ok().put("data", data);
+    }
+
+
+    /**
+     * 根据文章ID获取活动详情
+     */
+    @ApiOperation("根据文章ID获取活动详情")
+    @PostMapping("/getEventDetailByPostId")
+    public R getEventDetailByPostId(@RequestBody Map<String, Object> params) throws IOException {
+        String postId = StrUtil.toString(params.get("postId"));
+        CloseableHttpClient client = HttpClients.createDefault();
+        String url = "https://bbs-api.mihoyo.com/post/wapi/getPostFull?read=1&gids=2&post_id=" + postId;
+
+        Map<String, String> query = new HashMap<>();
+        query.put("read", "1");
+        query.put("gids", "2");
+        query.put("post_id", postId);
+
+        HttpGet httpGet = new HttpGet(url);
+
+        httpGet.setHeader("Accept-Encoding", "gzip, deflate, br");
+        httpGet.setHeader("Accept-Language", "zh-cn");
+        httpGet.setHeader("Connection", "keep-alive");
+        httpGet.setHeader("Host", "api-takumi.mihoyo.com");
+        httpGet.setHeader("x-rpc-app_version", "2.11.0");
+        httpGet.setHeader("x-rpc-client_type", "4");
+//        headers.put("Cookie", cookie);
+        httpGet.setHeader("Referer", "https://bbs.mihoyo.com/");
+        httpGet.setHeader("DS", MyUtils.GetDS(query, ""));
+
+        HttpResponse response = client.execute(httpGet);
+        HttpEntity entity = response.getEntity();
+        String str = EntityUtils.toString(entity);
+        return R.ok().put("data", str);
     }
 }
